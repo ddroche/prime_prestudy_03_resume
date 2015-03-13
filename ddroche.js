453d0587445c17f5b1cf7f2e366671d169a84ff9
Derek Roche <ddrochedsp@gmail.com>
2015-03-13 14:12:07 -0500

    sticker layout

--- ddroche.js
+++ ddroche.js
@@ -14,13 +14,15 @@ var resumeItems = {
 
 };
 
-function makeUnorderedList(array) {
+function makeSkillsList(array) {
 	// Create list element
 	var list = document.createElement('ul');
+	list.className += "box";
 
 	for(var i = 0; i < array.length; i++) {
 		// Create list item;
 		var item = document.createElement('li');
+		item.className += "skill";
 
 		// Set item
 		item.appendChild(document.createTextNode(array[i]));
@@ -33,11 +35,32 @@ function makeUnorderedList(array) {
 	return list;
 };
 
+function makeLeadershipList(array) {
+	// Create list element
+	var list = document.createElement('ul');
+
+	for(var i = 0; i < array.length; i++) {
+		// Create list item;
+		var item = document.createElement('li');
+		item.className += "leader";
+
+		// Set item
+		item.appendChild(document.createTextNode(array[i]));
+
+		// Add to list
+		list.appendChild(item);
+	}
+
+	// Return list
+	return list;
+};
+
+
 // Add skills list
-document.getElementById('skills').appendChild(makeUnorderedList(resumeItems.skills));
+document.getElementById('skills').appendChild(makeSkillsList(resumeItems.skills));
 
 // Add Leadership list
-document.getElementById('leadership').appendChild(makeUnorderedList(resumeItems.leadership));
+document.getElementById('leadership').appendChild(makeLeadershipList(resumeItems.leadership));
 
 
 // Work History
@@ -45,7 +68,7 @@ var jobs = [
 	{	// jobs[0]
 		"business"	: "Boise Inc.",
 		"title"		: "Co-op Engineer",
-		"dates"	: "January 2011 - January 2012",
+		"dates"		: "January 2011 - January 2012",
 		"duties"	: [
 						"Utilized CAD modeling / simulation and mathematical modeling software",
 						"Assisted with ISO 9001 re-certification",
@@ -59,7 +82,7 @@ var jobs = [
 	{	// jobs[1]
 		"business"	: "Best Western Copper Crown",
 		"title"		: "Front Desk Clerk",
-		"dates"	: "May 2009 - December 2010",
+		"dates"		: "May 2009 - December 2010",
 		"duties"	: [
 						"Completed the night audit and documentation",
 						"Provided a welcoming customer-oriented environment for guests"
@@ -68,7 +91,7 @@ var jobs = [
 	{	// jobs[2]
 		"business"	: "Shopko",
 		"title"		: "Electronics Sales Floor Team Member",
-		"dates"	: "September 2009 - September 2010",
+		"dates"		: "September 2009 - September 2010",
 		"duties"	: [
 						"Inform customers of electronics products",
 						"Sell product warranties to customers",
@@ -79,7 +102,7 @@ var jobs = [
 	{	// jobs[3]
 		"business"	: "Intermet Hibbing Foundry",
 		"title"		: "Summer General Labor",
-		"dates"	: "June 2007 - August 2008",
+		"dates"		: "June 2007 - August 2008",
 		"duties"	: [
 						"Ensured quality parts were ready to be inspected and shipped",
 						"Prepared boxes for shipping",
@@ -90,78 +113,76 @@ var jobs = [
 
 // function to display jobs from JSON object
 function displayJobs(array) {
-	var display = '<h1>Experience</h1>';
+	var display = '';
+	var boxClass = ["Business", "Title", "Dates", "Duties"];
 	// iterate over each key
 
 	for(var i = 0; i < array.length; i++) {
-			display += '<p class="business">' + array[i].business + '</p>' +
-					   	   '<p class="title">' + array[i].title + '</p>' +
-					       '<p class="dates">' + array[i].dates + '</p>';
+			display += 	   '<div class="box-item no-stitch box' + boxClass[i] + ' "><div class="box exp-box">' + 
+						   '<h2 class="business">' + array[i].business + '</h2>' +
+					   	   '<h3 class="title">' + array[i].title + '</h3>' +
+					       '<p class="dates">' + array[i].dates + '</p>' + '<hr width="80%">';
+					     
 			// iterate over each "duty" in duties array
 			for(var n = 0; n < array[i].duties.length; n++){
-			display += '<p class="duties">' + array[i].duties[n] + '</p>';
-		};
+			display += 	   '<p class="job-duties">'   + array[i].duties[n] + '</p>';
+			};
+			display +=	   '</div></div>'
 	};
-	document.getElementById('work').innerHTML = display;
+	document.getElementById('exp').innerHTML = display;
 };
 
 displayJobs(jobs);
-
-//document.getElementById('work').appendChild(displayJobs(jobs));
-
-// function displayJobs(array) {
-// 	// iterate over each key
-// 	for(var i = 0; i < array.length; i++) {
-// 			document.write('<p class="business">' + array[i].business + '</p>' +
-// 					   	   '<p class="title">' + array[i].title + '</p>' +
-// 					       '<p class="empDates">' + array[i].empDates + '</p>');
-// 			// iterate over each "duty" in duties array
-// 			for(var n = 0; n < array[i].duties.length; n++){
-// 			document.write('<p class="duties">' + array[i].duties[n] + '</p>');
-// 		};
-// 	};
-// };
 
 var involvement = [
 	{	// involved[0]
 		"title" 	  : "New Member Educator",
 		"dates" 	  : "September 2010 - December 2010",
-		"duties" : "Conducted New Member Orientation while teaching the best traditions and values of the Delta Sigma Phi National Engineered Leadership Fraternity"
+		"duties"	  : "Conducted New Member Orientation while teaching the best traditions and values of the Delta Sigma Phi National Engineered Leadership Fraternity"
 	},
 	{   // involved[1]
 		"title"		  : "Vice President - Recruitment",
-		"dates"	  : "January 2009 - January 2010",
-		"duties" : "Designed and oversaw a productive recruitment program"
+		"dates"		   : "January 2009 - January 2010",
+		"duties"	  : "Designed and oversaw a productive recruitment program"
 	},
 	{   // involved[2]
 		"title"		  : "Assistant Interfraternity Council Recruitment Director",
-		"dates"	  : "January 2009 - January 2010",
-		"duties" : "Organized campus-wide recruitment efforts for the Greek Life community"
+		"dates"		   : "January 2009 - January 2010",
+		"duties"	  : "Organized campus-wide recruitment efforts for the Greek Life community"
 	},
 	{   // involved[3]
 		"title"		  : "Interfraternity Council Representative",
-		"dates"	  : "September 2007 - January 2010",
-		"duties" : "Represented the chapter within a council of 22 organizations"
+		"dates"	 	  : "September 2007 - January 2010",
+		"duties"	  : "Represented the chapter within a council of 22 organizations"
 	},
 	{   // involved[4]
 		"title"		  : "Philanthropy Chair",
-		"dates"	  : "September 2008 - December 2009",
-		"duties" : "Engaged the chapter members in service work for the betterment of the community"
+		"dates"	 	  : "September 2008 - December 2009",
+		"duties"	  : "Engaged the chapter members in service work for the betterment of the community"
 	}
 
 ];
 
 function displayInvolvement(array) {
-	var display = '<h1>Involvement</h1>';
-	display += '<p class="organization">Delta Sigma Phi - National Engineered Leadership Fraternity</p>';
+	var displayOne = '<div class="box-item box">' + 
+			 	     '<h2 class="business">Delta Sigma Phi</h2>' + 
+			 	     '<h4>National Engineered Leadership Fraternity</h4>';
+	
+	document.getElementById('invSidebar').innerHTML = displayOne;
+
+	var displayTwo = '';
+
 	// iterate over each key
 
 	for(var i = 0; i < array.length; i++) {
-			display += '<p class="title">' + array[i].title + '</p>' +
-					   	   '<p class="dates">' + array[i].dates + '</p>' +
+			displayTwo +=  '<div class="box exp-box no-stitch">' +
+						   '<h3 class="title">' + array[i].title + '</h3>' +
+					   	   '<p class="dates">' + array[i].dates + '</p>' + '<hr width="80%">' +
 					       '<p class="duties">' + array[i].duties + '</p>';
+			displayTwo +=  '</div>'
 	};
-	document.getElementById('involvement').innerHTML = display;
+	displayTwo += 	   	   '</div>';
+	document.getElementById('inv').innerHTML = displayTwo;
 };
 
 displayInvolvement(involvement);
